12ff3147d5a015d673cd7e0ffe8c2482236c0311
alguan47 <alguan@up.edu.ph>
2020-04-25 21:39:52 +0800

    Updates

---
+++ precache-manifest.e87dd439cf76a72a5d96f9dcfc8ffc2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fa72edf636698ad6ce545ffeda949f",
+    "url": "/AManFromTheProvince/BetterNoteApp/index.html"
+  },
+  {
+    "revision": "e82fbc972bc7e8c694fa",
+    "url": "/AManFromTheProvince/BetterNoteApp/static/css/main.5de7f335.chunk.css"
+  },
+  {
+    "revision": "668541e5c05441cd5a3f",
+    "url": "/AManFromTheProvince/BetterNoteApp/static/js/2.b56ce3a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AManFromTheProvince/BetterNoteApp/static/js/2.b56ce3a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e82fbc972bc7e8c694fa",
+    "url": "/AManFromTheProvince/BetterNoteApp/static/js/main.ae8db034.chunk.js"
+  },
+  {
+    "revision": "9e01ea761f667808ce5e",
+    "url": "/AManFromTheProvince/BetterNoteApp/static/js/runtime-main.175a014e.js"
+  }
+]);
